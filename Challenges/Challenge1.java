2bcbdf621ec2bbc1daddf55a5f4ee6ab5ea16d22
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-09-10 14:13:25 -0600

    Add challenge 1

--- Challenges/Challenge1.java
+++ Challenges/Challenge1.java
@@ -26,13 +26,13 @@ public class Challenge1 {
         System.out.println("Pick a value for Saturday's temperature:");
         String strSat = scanner.next();
 
-        int sun=Integer.parseInt(strSun);
-        int mon=Integer.parseInt(strMon);
-        int tue=Integer.parseInt(strTue);
-        int wed=Integer.parseInt(strWed);
-        int thur=Integer.parseInt(strThur);
-        int fri=Integer.parseInt(strFri);
-        int sat=Integer.parseInt(strSat);
+        int sun = Integer.parseInt(strSun);
+        int mon = Integer.parseInt(strMon);
+        int tue = Integer.parseInt(strTue);
+        int wed = Integer.parseInt(strWed);
+        int thur = Integer.parseInt(strThur);
+        int fri = Integer.parseInt(strFri);
+        int sat = Integer.parseInt(strSat);
         int avrg = ((sun + tue + wed + thur + fri + sat) / 7);
 
         System.out.println("\nSunday's temperature: " + sun + "\n\nMonday's temperature: " + mon + "\n\nTuesday's temperature: " + tue + "\n\nWednesday's temperature: " + wed + "\n\nThrusday's temperature: " + thur + "\n\nFriday's temperature: " + fri + "\n\nSaturday's temperature: " + sat + "\n\nWeekly Average temperature: " + avrg);
