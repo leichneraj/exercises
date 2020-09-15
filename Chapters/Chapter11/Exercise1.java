8083b46b453aa05a05134ce1e87ebc1538e70c5d
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-09-15 14:49:38 -0600

    Add chapter 11 exercises.

---
+++ Chapters/Chapter11/Exercise1.java
@@ -0,0 +1,11 @@
+public class Exercise1 {
+
+  public static void main ( String[] args ) { 
+
+    String str;
+    str = new String("Hello World");
+    String secondVariable;
+    System.out.println(str);
+    System.out.println(secondVariable);
+  }
+}
---
+++ Chapters/Chapter11/StringDemo2.java
@@ -0,0 +1,13 @@
+public class StringDemo2 {
+  public static void main ( String[] args ) {
+    String str;
+    int    len;
+
+    str = new String( "So the mouse once said to elephant, 'BOO'!" );
+
+    len = str.length();
+
+    System.out.println("The length is: " + len );
+
+  }
+}
