dec48e6bffbe0d17600fa382f689337015cc2282
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-10-07 10:54:47 -0600

    Snake Eyes game with statistics output.

--- Challenges/SnakeEyes.java
+++ Challenges/SnakeEyes.java
@@ -1,33 +1,35 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.AbstractMap.SimpleImmutableEntry;
 
 public class SnakeEyes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random randomGenerator = new Random();
 
-        System.out.print("Enter value for how many times the dice must roll: ");
-        int simulations = scanner.nextInt();
-        scanner.close();
-
-        int score = 0;
-        boolean stop = false;
-
-        while(simulations >= 1) {
-            int dice1 = randomGenerator.nextInt(6);
-            int dice2 = randomGenerator.nextInt(6);
-            if(dice1 != dice2) {
-                score = score + dice1 + dice2;
-            } else if(!stop) {
-                int finishedScore = score;
-                System.out.println("Your score before Snake Eyes was: " + finishedScore);
-                stop = true;
-            }
-            simulations--;
-        }
-
+        System.out.print("How many times do you wish to roll? ");
+        int rolls = scanner.nextInt();
+        System.out.println(values(rolls));
     }
-        
+
+    public static int values(int rolls) {
+        Random randomGenerator = new Random();
+        int score = 0;
+
+        while (rolls > 0) {
+            int randomNumber1 = randomGenerator.nextInt(6) + 1;
+            int randomNumber2 = randomGenerator.nextInt(6) + 1;
+
+            if(randomNumber1 != randomNumber2) {
+                rolls--;
+                score = score + randomNumber1 + randomNumber2;
+                if(rolls == 0) {
+                    System.out.println("\nNo Snake Eyes.");
+                }
+            } else {
+                int rollsToSnakeEyes = rolls;
+            }
+        }
+        return ;
+    }
+
 }
