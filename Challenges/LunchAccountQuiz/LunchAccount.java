aaf78d5705e592da46024480f69f4cb95b9f3ea3
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-11-14 12:42:00 -0700

    Express data for a student and complete transaction processes.

--- Challenges/LunchAccountQuiz/LunchAccount.java
+++ Challenges/LunchAccountQuiz/LunchAccount.java
@@ -1,17 +1,32 @@
+import java.text.DecimalFormat;
+
 public class LunchAccount {
     
-    private double bal;
-    private int id;
+    private double bal, lifetimeSpent;
+    private int id, lunchesBought;
     private String name;
-    private static int studentCount = 1000;
+    private static int studentCount = 0;
 
     public LunchAccount(String name, double bal) {
         this.name = name;
         this.bal = bal;
-        id = studentCount;
+        id = studentCount + 1000;
         studentCount++;
         if(id <= 1100) {
             this.bal = this.bal + 20.00;
+        }
+    }
+
+    public String purchase(double amount) {
+        DecimalFormat rounded = new DecimalFormat("###.##");
+        this.bal = this.bal - amount;
+        if(this.bal < 0 ) {
+            this.bal = this.bal + amount;
+            return "Insufficient funds, purchase denied. Needs " + rounded.format(-1 * (this.bal - amount)) + " more.";
+        } else {
+            lifetimeSpent = lifetimeSpent + amount;
+            lunchesBought++;
+            return "Transaction completed.";
         }
     }
 
@@ -27,16 +42,12 @@ public class LunchAccount {
         return this.bal;
     }
 
-    public void addBal(double addition) {
-        this.bal = this.bal + addition;
+    public String getData() {
+        return ("\nSpent since joining: $" + lifetimeSpent + "\nLunches bought: " + lunchesBought);
     }
 
-    public void purchase(double amount) {
-        if(amount > 0) {
-            this.bal = this.bal - amount;
-        } else if(amount < 0) {
-            this.bal = this.bal + amount;
-        }
+    public void addBal(double addition) {
+        this.bal = this.bal + addition;
     }
 
 }
--- Challenges/LunchAccountQuiz/LunchAccountTester.java
+++ Challenges/LunchAccountQuiz/LunchAccountTester.java
@@ -3,8 +3,13 @@ public class LunchAccountTester {
     public static void main(String[] args) {
         LunchAccount joe = new LunchAccount("joe", 20.00); // Creates new student with an initial balance
 
-        joe.addBal(10.00); // Adds 10.02 to total
+        joe.addBal(10.00); // Adds 10.00 to total
         System.out.println(joe.getName() + " (" + joe.getId() + ") Balance: \n$" + joe.getBal()); // Expecting: joe (1000) Balance: $50.0
+
+        System.out.println("\n" + joe.purchase(12.34));
+        System.out.println(joe.getName() + " (" + joe.getId() + ") Balance: \n$" + joe.getBal()); // Expecting: joe (1000) Balance: $37.66
+
+        System.out.println(joe.getData()); // Expecting: Spent since joining: $12.34 (\n) Lunches Bought: 1
     }
     
 }
