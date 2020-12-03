7a154c723f6fdb186e9879d2faf62578a95b4857
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-12-03 12:06:17 -0700

    Calculate time for each food item in a microwave.

--- Chapters/Chapter17/Exercise3.java
+++ Chapters/Chapter17/Exercise3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercise2 {
+public class Exercise3 {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
