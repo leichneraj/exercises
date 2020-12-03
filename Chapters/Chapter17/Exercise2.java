4c775502d0881d68a515e02b2ce9aa8c760f9912
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-12-03 12:03:57 -0700

    Calculate time for each food item in a microwave.

---
+++ Chapters/Chapter17/Exercise2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean overThree = false;
+        int minutes = 0;
+
+        System.out.print("Number of items: ");
+        int items = scanner.nextInt();
+        System.out.print("Time for one item: ");
+        int seconds = scanner.nextInt();
+        
+        if(items > 3) {
+            System.out.println("Only three items allowed.");
+            overThree = true;
+        } else if(items == 3) {
+            seconds = seconds * 2;
+        } else if(items == 2) {
+            seconds = seconds + (seconds / 2);
+        }
+
+        if(seconds > 60) {
+            minutes = seconds / 60;
+            seconds = seconds - (minutes * 60);
+        }
+
+        if(overThree == false) {
+            System.out.println("\nHeat for " + minutes + " minute(s) and " + seconds + " seconds.");
+        }
+    }
+}
